5ecff85a901463b1b84e1923728a098fcf01dd86
Josh Bodah <jb3689@yahoo.com>
2014-11-21 09:59:34 -0500

    small refactoring + improve docs

--- lib/spy.rb
+++ lib/spy.rb
@@ -8,13 +8,23 @@ module Spy
   module ClassMethods
     include Errors
 
+    # Initializes a new spy instance for the method
+    #
+    # @param receiver - the receiver of the message you want to spy on
+    # @param msg - the message passed to the receiver that you want to spy on
     def on(receiver, msg)
       add_spy(receiver, msg)
     end
 
+    # Stops spying on the method and restores its original functionality
+    #
+    # @param args - supports multiple signatures
+    #               
+    #               Spy.restore(:all)           => stops spying on every spied message
+    #               Spy.restore(receiver, msg)  => stops spying on the given receiver and message
     def restore(*args)
-      if args.length == 1
-        spies.each {|k,v| restore(find_object(k), v.msg)} if args[0] == :all
+      if args.length == 1 && args[0] == :all
+        spies.each {|k,v| restore(find_object(k), v.msg)}
       elsif args.length == 2
         object, msg = *args
         spies[object.object_id].destroy
@@ -24,10 +34,12 @@ module Spy
 
     private
 
+    # Looks up an object in the global ObjectSpace
     def find_object(object_id)
       ObjectSpace._id2ref(object_id)
     end
 
+    # Global hash of known spies
     def spies
       @spies ||= {}
     end
