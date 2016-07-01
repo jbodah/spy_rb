e8a4bddb29fa14cda0f5dec7ab5212b64e7a84a6
Josh Bodah <jb3689@yahoo.com>
2016-07-01 09:34:07 -0400

    rubocop: use array literals

--- test/call_history_test.rb
+++ test/call_history_test.rb
@@ -3,20 +3,20 @@ require 'test_helper'
 class CallHistoryTest < Minitest::Spec
   describe 'Spy::Instance#call_history' do
     it 'is empty when no calls have been made' do
-      arr = Array.new
+      arr = []
       spy = Spy.on(arr, :<<)
       assert spy.call_history.empty?
     end
 
     it 'has a single item when one call has been made' do
-      arr = Array.new
+      arr = []
       spy = Spy.on(arr, :<<)
       arr << 'a'
       assert_equal 1, spy.call_history.size
     end
 
     it 'has two items, ordered from first call to last, when two calls have been made' do
-      arr = Array.new
+      arr = []
       spy = Spy.on(arr, :<<)
       arr << 'a'
       arr << 'b'
