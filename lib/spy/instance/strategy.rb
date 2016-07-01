d603e3ec92d71d94933b17dc6abfb3ba67fa7317
Josh Bodah <jb3689@yahoo.com>
2016-07-01 09:55:24 -0400

    simplify Spy::Instance::Strategy::factory_build

--- lib/spy/instance/strategy.rb
+++ lib/spy/instance/strategy.rb
@@ -7,17 +7,24 @@ module Spy
       class << self
         def factory_build(spy)
           if spy.original.is_a?(Method)
-            if spy.original.owner == spy.spied.singleton_class
-              Strategy::Wrap.new(spy)
-            else
-              Strategy::Intercept.new(spy, spy.spied.singleton_class)
-            end
+            pick_strategy(spy, spy.spied.singleton_class)
           else
-            if spy.original.owner == spy.spied
-              Strategy::Wrap.new(spy)
-            else
-              Strategy::Intercept.new(spy, spy.spied)
-            end
+            pick_strategy(spy, spy.spied)
+          end
+        end
+
+        private
+
+        def pick_strategy(spy, spied_on)
+          if spy.original.owner == spied_on
+            # If the object we're spying on is the owner of
+            # the method under spy then we need to wrap that
+            # method
+            Strategy::Wrap.new(spy)
+          else
+            # Otherwise we can intercept it by abusing the
+            # inheritance hierarchy
+            Strategy::Intercept.new(spy, spied_on)
           end
         end
       end
